8c86dd4f5f9b3075f18949d70d1c514974c1f7d6
rohit3607 <rohitplayer87089@gmail.com>
2025-02-14 18:53:01 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -1,61 +1,45 @@
-#    This file is part of the AutoAnime distribution.
-#    Copyright (c) 2025 Kaif_00z
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
-
-# if you are using this following code then don't forgot to give proper
-# credit to t.me/kAiF_00z (github.com/kaif-00z)
-
 from decouple import config
 
 
 class Var:
     # Version
-
     __version__ = "v0.0.8"
 
     # Telegram Credentials
-
     API_ID = config("API_ID", default=22469064, cast=int)
     API_HASH = config("API_HASH", default="c05481978a217fdb11fa6774b15cba32")
-    BOT_TOKEN = config("BOT_TOKEN", default=7542241757:AAFgeHI2tr518Hbh8DOvLIfeCpQj0udfk-Y)
+    BOT_TOKEN = config("BOT_TOKEN", default="7542241757:AAFgeHI2tr518Hbh8DOvLIfeCpQj0udfk-Y")
     SESSION = config("SESSION", default=None)
 
     # Database Credentials
-
-    MONGO_SRV = config("MONGO_SRV", default=mongodb+srv://rohitplayer87089:rohit870@cluster0.4wt927p.mongodb.net/?retryWrites=true&w=majority)
+    MONGO_SRV = config(
+        "MONGO_SRV",
+        default="mongodb+srv://rohitplayer87089:rohit870@cluster0.4wt927p.mongodb.net/?retryWrites=true&w=majority"
+    )
 
     # Channels Ids
-
     BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=-1002370816678, cast=int)
-    MAIN_CHANNEL = config("MAIN_CHANNEL", -1002370816678, cast=int)
-    LOG_CHANNEL = config("LOG_CHANNEL", -1002370816678, cast=int)
-    CLOUD_CHANNEL = config("CLOUD_CHANNEL", -1002370816678, cast=int)
+    MAIN_CHANNEL = config("MAIN_CHANNEL", default=-1002370816678, cast=int)
+    LOG_CHANNEL = config("LOG_CHANNEL", default=-1002370816678, cast=int)
+    CLOUD_CHANNEL = config("CLOUD_CHANNEL", default=-1002370816678, cast=int)
     FORCESUB_CHANNEL = config("FORCESUB_CHANNEL", default=-1002370816678, cast=int)
     OWNER = config("OWNER", default=0, cast=int)
 
     # Other Configs
-
     THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/ad1b25807b81cdf1dff65.jpg"
+        "THUMBNAIL",
+        default="https://graph.org/file/ad1b25807b81cdf1dff65.jpg"
     )
     FFMPEG = config("FFMPEG", default="ffmpeg")
     CRF = config("CRF", default="27")
     SEND_SCHEDULE = config("SEND_SCHEDULE", default=True, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
     LOG_ON_MAIN = config("LOG_ON_MAIN", default=False, cast=bool)
-    FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="https://t.me/+tKRLKu4T6Kc4N2E1", cast=str)
+    FORCESUB_CHANNEL_LINK = config(
+        "FORCESUB_CHANNEL_LINK",
+        default="https://t.me/+tKRLKu4T6Kc4N2E1",
+        cast=str
+    )
 
     # Dev Configs
-
     DEV_MODE = config("DEV_MODE", default=False, cast=bool)
